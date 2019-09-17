38d08bec1c9bf8c7f90fa03c86e297513d52e32d
Matiaslys <mati1857@edu.easj.dk>
2019-09-17 10:30:04 +0200

    I have made some Test cases

---
+++ src/Opgave_3.java
@@ -0,0 +1,15 @@
+public class Opgave_3 {
+    public static void main(String[] args) {
+        //Test case hvor sidste værdi er midterst
+        //Input: a = 6, b = 10, c = 8
+        //Forventet output: 8
+
+        //Test case hvor midterste værdi er midterst
+        //Input: a = 3, b = 7, c = 2
+        //Forventet output 3
+
+        //Test case hvor midterste værdi er midterst
+        //Input: a = 10, b = 25, c = 50
+        //Forventet output = 25
+    }
+}
